6dd62ccfb0a331f585033cfe5b8629b656f7f4b3
shayan esfahanizadeh <106907105+Shayan-esf@users.noreply.github.com>
2022-07-30 16:49:57 +0430

    This is a training program to work with GitHub API

---
+++ githubfinder/github.js
@@ -0,0 +1,23 @@
+class Github {
+  constructor() {
+    // take in github
+    this.client_id = '';
+    this.client_secret = '1be93bca0268d2243b7539f0cdb6c89579217b23';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
